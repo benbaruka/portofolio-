5df11f986d35265072d1814e80bc5129e1df3e07
ickah <barukab68@gmail.com>
2024-01-06 10:32:12 +0100

    new project

---
+++ src/feature/layout/Header.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/src/theme/ThemeToggle";
+import React from "react";
+
+export const Header = async () => {
+  return (
+    <header className=" border-b border-b-accent">
+      <div className=" container flex items-center py-8 m-auto gap-1 ">
+        <h2 className=" text-xl font-bold mr-auto">B. JETHRO</h2>
+        <div className=" flex ">
+          <Button>Me joindre</Button>
+          <ThemeToggle />
+        </div>
+        <div className=" flex gap-8">
+          <Button>Me joindre</Button>
+          <ThemeToggle />
+        </div>
+      </div>
+    </header>
+  );
+};
